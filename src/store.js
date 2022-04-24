2f52e8aa0d4e78480ea2223866f54560e382c73f
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2022-04-24 07:00:19 +0530

    cm28

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 import { combineReducers, createStore } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as UsersReducer } from "./Redux/users/reducer";
 import { reducer as FeedsReducer } from "./Redux/feeds/reducer";
 import { reducer as JobsReducer } from "./Redux/Jobs/reducer";
@@ -11,5 +10,5 @@ const rootReducer = combineReducers({
     commentsState: CommentsReducer
 });
 
-export const store = createStore(rootReducer, composeWithDevTools() );
+export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__() );
 console.log(store.getState());
