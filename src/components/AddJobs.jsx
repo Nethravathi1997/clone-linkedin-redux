0e97d763a667507633e339fa8bfe449006453169
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2022-04-30 20:42:20 +0530

    Update AddJobs.jsx

--- src/components/AddJobs.jsx
+++ src/components/AddJobs.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import "./addJobs.css";
