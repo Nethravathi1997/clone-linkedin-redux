f793650776cf70a22377885cc34f6081d4389fd8
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2023-07-11 14:14:21 +0530

    Update actionTypes.js

--- src/Redux/Jobs/actionTypes.js
+++ src/Redux/Jobs/actionTypes.js
@@ -1,3 +1,7 @@
 export const ADD_JOB = "ADD_JOB";
 export const REMOVE_JOB = "REMOVE_JOB";
 
+export const ADD_JOB_LOADING = "ADD_JOB_LOADING";
+export const ADD_JOB_SUCCESS = "ADD_JOB_SUCCESS";
+export const ADD_JOB_ERROR = "ADD_JOB_ERROR";
+
